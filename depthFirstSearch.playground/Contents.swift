8e2d4607174ecf4541095e22415fd0ee5aff18dc
jakouk <jakoriaty@gmail.com>
2018-06-02 01:08:31 +0900

    Add depthFirstSearch

---
+++ depthFirstSearch.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public class BinaryTreeNode<T: Comparable> {
+  // Value와 자식 변수
+  public var value: T
+  public var leftChild: BinaryTreeNode?
+  public var rightChild: BinaryTreeNode?
+  public var parent: BinaryTreeNode?
+  
+  // 초기화
+  public convenience init(value:T) {
+    self.init(value: value, left: nil, right: nil, parent: nil)
+  }
+  
+  public init(value: T, left: BinaryTreeNode?, right: BinaryTreeNode?, parent: BinaryTreeNode? ) {
+    self.value = value
+    self.leftChild = leftChild
+    self.rightChild = rightChild
+    self.parent = parent
+  }
+  
+  public class func depthFirstSearch(node: BinaryTreeNode?) {
+    // Nil인 잎에 도달하면 재귀적인 호출을 중지함
+    guard let node = node else {
+      return
+    }
+    BinaryTreeNode.depthFirstSearch(node: node.leftChild)
+    print(node.value)
+    BinaryTreeNode.depthFirstSearch(node: node.rightChild)
+  }
+}
